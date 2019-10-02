671273a0ee2dca68e5783d66ba95e0580764e8d9
ivanzy <idimitryz@gmail.com>
2019-10-02 15:13:05 -0300

    adicinando exercicio de filas na unity

--- queue/Program.cs
+++ queue/Program.cs
@@ -1,10 +1,46 @@
 ﻿using System;
-
+using System.Collections;
 namespace queue
 {
    class Program
    {
       static void Main(string[] args)
+      {
+         Queue fila = new Queue();
+         fila.Enqueue("elemento 1");
+         fila.Enqueue("elemento 2");
+         fila.Enqueue("elemento 3");
+         fila.Enqueue("elemento 4");
+         Console.WriteLine(fila.Dequeue());
+         Console.WriteLine(fila.Dequeue());
+         fila.Enqueue("elemento 5");
+         Console.WriteLine(fila.Dequeue());
+         fila.Enqueue("elemento 6");
+         fila.Enqueue("elemento 7");
+         fila.Enqueue("elemento 8");
+         Console.WriteLine(fila.Dequeue());
+         Console.WriteLine(fila.Dequeue());
+         Console.WriteLine(fila.Dequeue());
+         Console.WriteLine(fila.Dequeue());
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      static void ArrayQueueFunction()
       {
          QueueArray fila = new QueueArray(5);
          fila.Enqueue(1);
@@ -21,9 +57,6 @@ namespace queue
          fila.Enqueue(6);
 
          Console.WriteLine(fila.Dequeue());
-
-
-
       }
    }
 
